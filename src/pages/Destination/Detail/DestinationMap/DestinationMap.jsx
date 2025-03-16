3fc804f5a57564f2247b3394e15663191f53d388
Yoonhwi <ush0105@naver.com>
2025-03-16 21:50:12 +0900

    chore: 여행지페이지 지도의 zoom값 변경

--- src/pages/Destination/Detail/DestinationMap/DestinationMap.jsx
+++ src/pages/Destination/Detail/DestinationMap/DestinationMap.jsx
@@ -54,7 +54,7 @@ const DestinationMap = ({ latitude, longitude, restaurants }) => {
           <Map
             style={{ width: "100%", height: "100%" }}
             defaultCenter={position}
-            defaultZoom={13}
+            defaultZoom={12}
             gestureHandling={"greedy"}
             disableDefaultUI={true}
             mapId="fine_place"
