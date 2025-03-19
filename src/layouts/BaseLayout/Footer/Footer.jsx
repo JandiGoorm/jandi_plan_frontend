36dc50740defac151d0fcf7e7ba58456a5efad25
Yoonhwi <ush0105@naver.com>
2025-03-19 14:52:09 +0900

    feat: Footer에 개인정보처리방침 이미지 추가

--- src/layouts/BaseLayout/Footer/Footer.jsx
+++ src/layouts/BaseLayout/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+import { Modal, ModalContent, ModalTrigger } from "@/components";
 import Contact from "./Contact";
 import styles from "./Footer.module.css";
 import TechStack from "./TechStack";
@@ -11,8 +12,20 @@ const Footer = () => {
       </div>
 
       <div className={styles.notice_container}>
-        <p className={styles.policy}>이용정책</p>
-        <p className={styles.policy}>개인정보처리방침</p>
+        <Modal>
+          <ModalTrigger>
+            <p className={styles.policy}>개인정보처리방침</p>
+          </ModalTrigger>
+          <ModalContent>
+            <div className={styles.policy_container}>
+              <img
+                src="/information-policy.webp"
+                alt="information-policy"
+                className={styles.policy_image}
+              />
+            </div>
+          </ModalContent>
+        </Modal>
         <p className={styles.title}>© 2025 Groom - Jandi TEAM</p>
       </div>
     </footer>
