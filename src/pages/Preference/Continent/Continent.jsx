d0ea478958df48dba4196dfcb032492b4fb72c2b
Yoonhwi <ush0105@naver.com>
2025-03-21 00:10:29 +0900

    refactor: preference관련 리팩토링

--- src/pages/Preference/Continent/Continent.jsx
+++ src/pages/Preference/Continent/Continent.jsx
@@ -1,31 +1,28 @@
-import { useState,useEffect } from "react";
 import styles from "./Continent.module.css";
-import { useNavigate, useLocation  } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import { Button, Loading } from "@/components";
 import { PageEndPoints, APIEndPoints } from "@/constants";
 import { FaCheck } from "react-icons/fa";
 import { useAxios } from "@/hooks";
-import { useAuth, useToast } from "@/contexts";
-
+import { useToast } from "@/contexts";
 
 const Continent = () => {
   const [selectedContinents, setSelectedContinents] = useState([]);
-  const [continents, setContinents] = useState([]);
+
   const navigate = useNavigate();
   const { createToast } = useToast();
-  const { loading, fetchData } = useAxios();
+  const { loading, fetchData, response } = useAxios();
   const location = useLocation();
   const mode = location.state?.mode;
 
   useEffect(() => {
     fetchData({
       method: "GET",
-      url: `${APIEndPoints.CONTINENT}`,
-      params: {filter: ""},
-    }).then((res) => {
-      setContinents(res.data);
+      url: APIEndPoints.CONTINENT,
+      params: { filter: "" },
     });
-  }, []);
+  }, [fetchData]);
 
   const handleContinentClick = (continentName) => {
     setSelectedContinents((prevSelected) => {
@@ -38,56 +35,52 @@ const Continent = () => {
   };
 
   const handleNextClick = () => {
-    console.log(selectedContinents);
     {
       selectedContinents.length === 0
-        ? createToast({ type: "error", text: "관심있는 대륙을 선택해 주세요."})
-        : navigate(PageEndPoints.PREF_DEST, { state: { selectedContinents, mode } });
+        ? createToast({ type: "error", text: "관심있는 대륙을 선택해 주세요." })
+        : navigate(PageEndPoints.PREF_DEST, {
+            state: { selectedContinents, mode },
+          });
     }
   };
 
+  if (loading) return <Loading />;
   return (
-    <>
-      {loading ? (
-        <Loading />
-      ) : (
-      <div className={styles.container}>
-        <p className={styles.title}>관심있는 여행지가 어디신가요?</p>
+    <div className={styles.container}>
+      <p className={styles.title}>관심있는 여행지가 어디신가요?</p>
 
-        <div className={styles.continent_box}>
-          {continents.map((item) => {
-            const isSelected = selectedContinents.includes(item.name);
-            return (
-              <div
-                key={item.name}
-                className={styles.continent}
-                onClick={() => handleContinentClick(item.name)}
-              >
-                <img
-                  src={item.imageUrl}
-                  alt="continent"
-                  className={`${styles.cont_img} ${
-                    isSelected ? styles.selected_img : ""
-                  }`}
-                />
-                {isSelected ? <FaCheck className={styles.check_box} /> : null}
-                {isSelected ? (
-                  <div className={styles.selected_text}>{item.name}</div>
-                ) : (
-                  <div className={styles.hover_text}>{item.name}</div>
-                )}
-              </div>
-            );
-          })}
-        </div>
-        <div className={styles.button_box}>
-          <Button size="lg" variant="outline" onClick={() => handleNextClick()}>
-            다음
-          </Button>
-        </div>
+      <div className={styles.continent_box}>
+        {response?.map((item) => {
+          const isSelected = selectedContinents.includes(item.name);
+          return (
+            <div
+              key={item.name}
+              className={styles.continent}
+              onClick={() => handleContinentClick(item.name)}
+            >
+              <img
+                src={item.imageUrl}
+                alt="continent"
+                className={`${styles.cont_img} ${
+                  isSelected ? styles.selected_img : ""
+                }`}
+              />
+              {isSelected ? <FaCheck className={styles.check_box} /> : null}
+              {isSelected ? (
+                <div className={styles.selected_text}>{item.name}</div>
+              ) : (
+                <div className={styles.hover_text}>{item.name}</div>
+              )}
+            </div>
+          );
+        })}
       </div>
-    )}
-  </>
+      <div className={styles.button_box}>
+        <Button size="lg" variant="outline" onClick={() => handleNextClick()}>
+          다음
+        </Button>
+      </div>
+    </div>
   );
 };
 
--- src/pages/Preference/Destination/PrefDestination.jsx
+++ src/pages/Preference/Destination/PrefDestination.jsx
@@ -1,197 +1,219 @@
 import styles from "./PrefDestination.module.css";
 import { useLocation, useNavigate } from "react-router-dom";
-import { useState,useEffect } from "react";
-import { Button, Loading } from "@/components";
-import { PageEndPoints,APIEndPoints } from "@/constants";
+import { useState, useEffect } from "react";
+import { Button } from "@/components";
+import { PageEndPoints, APIEndPoints } from "@/constants";
 import { FaCheck } from "react-icons/fa";
 import { BiSolidPlaneAlt } from "react-icons/bi";
-import { useAuth, useToast } from "@/contexts";
+import { useToast } from "@/contexts";
 import { useAxios } from "@/hooks";
 
 const PrefDestination = () => {
-    const navigate = useNavigate();
-    const { state } = useLocation();
-    const { loading, fetchData } = useAxios();
-    const { createToast } = useToast();
-    const [destinations, setDestinations] = useState([]); //필터링 전체 도시 데이터
-    const [selectedDestinations, setSelectedDestinations] = useState([]);
-    const [currentIndex, setCurrentIndex] = useState(0); 
-    const [selectedContinents] = useState(state?.selectedContinents || []);
-    const [mode] = useState(state?.mode || []);
-    
-    useEffect(() =>{
-        const params = new URLSearchParams();
-        params.append("category", "CONTINENT");
-        if (selectedContinents.length > 0) {
-            params.append("filter", selectedContinents.join(","));
-        }
+  const navigate = useNavigate();
+  const { state } = useLocation();
+  const { fetchData, response } = useAxios();
+  const { createToast } = useToast();
 
-        fetchData({
-            method: "GET",
-            url: `${APIEndPoints.DESTINATION}`,
-            params,
-        }).then((res)=>{
-            console.log(res.data);
-            setDestinations(res.data || []);
+  const [selectedDestinations, setSelectedDestinations] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [selectedContinents] = useState(state?.selectedContinents || []);
+  const [mode] = useState(state?.mode || []);
+
+  const currentContinent = selectedContinents[currentIndex];
+
+  // 현재 선택된 대륙의 도시 필터링
+  const filteredDestinations =
+    response ??
+    [].filter(
+      (destination) => destination.country.continent.name === currentContinent
+    );
+
+  // 대륙 도시 내 국가별 그룹화화
+  const groupedDestinations = filteredDestinations.reduce(
+    (acc, destination) => {
+      const { country } = destination;
+      if (!acc[country.countryId]) {
+        acc[country.countryId] = {
+          countryName: country.name,
+          destinations: [],
+        };
+      }
+      acc[country.countryId].destinations.push(destination);
+      return acc;
+    },
+    {}
+  );
+
+  const handleComplete = () => {
+    if (selectedDestinations.length === 0) {
+      createToast({ type: "error", text: "관심있는 여행지를 선택해 주세요." });
+      return;
+    }
+
+    if (mode === "create") {
+      fetchData({
+        method: "POST",
+        url: APIEndPoints.PREFER_DEST,
+        data: {
+          cities: selectedDestinations,
+        },
+      })
+        .then(() => {
+          createToast({
+            type: "success",
+            text: "관심가는 여행지를 저장하였습니다.",
+          });
+          navigate(PageEndPoints.HOME);
         })
-    }, [fetchData]);
-
-    const currentContinent = selectedContinents[currentIndex];
-    // 현재 선택된 대륙의 도시 필터링
-    const filteredDestinations = destinations.filter(
-        (destination) => destination.country.continent.name === currentContinent
-    );
-    // 대륙 도시 내 국가별 그룹화화
-    const groupedDestinations = filteredDestinations.reduce((acc, destination) => {
-        const { country } = destination;
-        if (!acc[country.countryId]) {
-            acc[country.countryId] = {
-                countryName: country.name,
-                destinations: [],
-            };
-        }
-        acc[country.countryId].destinations.push(destination);
-        return acc;
-    }, {});
-
-    const handleNext = () => {
-        if (currentIndex < state?.selectedContinents.length - 1) {
-            setCurrentIndex(currentIndex + 1);
-        } else {
-            handleComplete();
-        }
-    };
-
-    const handleComplete = () => {
-        if (selectedDestinations.length === 0) {
-            createToast({ type: "error", text: "관심있는 여행지를 선택해 주세요."});
-            return;
-        }
-
-        if(mode==="create"){
-            fetchData({
-                method: "POST",
-                url: `${APIEndPoints.PREFER_DEST}`,
-                data: {
-                    cities: selectedDestinations,
-                },
-            }).then((res) =>{
-                createToast({
-                    type: "success",
-                    text: "관심가는 여행지를 저장하였습니다.",
-                });
-                navigate(PageEndPoints.HOME);
-            }).catch((err) =>{
-                createToast({
-                    type: "error",
-                    text: "관심가는 여행지를 저장하는데 실패하였습니다.",
-                });
-            })
-        }
-        else{
-            fetchData({
-                method: "PATCH",
-                url: `${APIEndPoints.PREFER_DEST}`,
-                data: {
-                    cities: selectedDestinations,
-                },
-            }).then((res) =>{
-                createToast({
-                    type: "success",
-                    text: "관심가는 여행지를 수정하였습니다.",
-                });
-                navigate(PageEndPoints.MYPAGE);
-            }).catch((err) =>{
-                createToast({
-                    type: "error",
-                    text: "관심가는 여행지를 수정하는데 실패하였습니다.",
-                });
-            })
-        }
-
-
-    };
-
-    const handlePrev = () => {
-        if (currentIndex === 0) {
-            navigate(-1);
-        } else {
-            setCurrentIndex(currentIndex - 1);
-        }
-    };
-
-    const handleSelectDestination = (destinationName) => {
-        setSelectedDestinations((prev) =>
-            prev.includes(destinationName)
-                ? prev.filter((name) => name !== destinationName)
-                : [...prev, destinationName]
+        .catch(() =>
+          createToast({
+            type: "error",
+            text: "관심가는 여행지를 저장하는데 실패하였습니다.",
+          })
         );
-    };
+    } else {
+      fetchData({
+        method: "PATCH",
+        url: APIEndPoints.PREFER_DEST,
+        data: {
+          cities: selectedDestinations,
+        },
+      })
+        .then(() => {
+          createToast({
+            type: "success",
+            text: "관심가는 여행지를 수정하였습니다.",
+          });
+          navigate(PageEndPoints.MYPAGE);
+        })
+        .catch(() =>
+          createToast({
+            type: "error",
+            text: "관심가는 여행지를 수정하는데 실패하였습니다.",
+          })
+        );
+    }
+  };
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.title_box}>
-                <p className={styles.title}>관심있는 여행지를 선택하세요.</p>
-            </div>
-
-            <div className={styles.destination_box}>
-            <h2 className={styles.continent_title}>{currentContinent}</h2>
-                    <div className={styles.continent_section}>
-                        <div className={styles.destination_list}>
-                            {Object.values(groupedDestinations).map((category) => (
-                            <div className={styles.category_section}>
-                                <h3 className={styles.category_title}>{category.countryName}</h3>
-                                <div className={styles.destination_items}>
-                                {category.destinations.map((destination) => {
-                                    const isSelected = selectedDestinations.includes(destination.name);
-                                    return (
-                                    <div
-                                        key={destination.name}
-                                        className={styles.destination}
-                                        onClick={() => handleSelectDestination(destination.name)}
-                                    >
-                                        <div className={styles.dest_trans}>                                        
-
-                                            <img
-                                            src={destination.imageUrl}
-                                            alt={destination.name}
-                                            className={`${styles.dest_img} ${isSelected ? styles.selected_img : ""}`}
-                                            />
-                                            {isSelected ? <BiSolidPlaneAlt className={styles.check_box}/>:null}
-
-                                            <img
-                                            src={destination.imageUrl}
-                                            alt={destination.name}
-                                            className={styles.dest_back_img}
-                                            />
-                                            {isSelected ? <FaCheck className={styles.check_box_back}/>:null}
-                                            <div className={styles.dest_back_box}>
-                                                <h1>{destination.description}</h1>
-                                            </div>
-                                        </div>
-                                        <div className={styles.destination_text}>{destination.name}</div>
-                                    </div>
-                                    );
-                                })}
-                                </div>
-                            </div>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-
-
-            <div className={styles.button_box}>
-                <Button size="lg" variant="outline" onClick={handlePrev}>
-                    이전
-                </Button>
-                <p className={styles.index_number}>{currentIndex+1} / {selectedContinents.length}</p>
-                <Button size="lg" variant="outline" onClick={handleNext}>
-                    {currentIndex === selectedContinents.length - 1 ? "완료" : "다음"}
-                </Button>
-            </div>
-        </div>
+  const handleSelectDestination = (destinationName) => {
+    setSelectedDestinations((prev) =>
+      prev.includes(destinationName)
+        ? prev.filter((name) => name !== destinationName)
+        : [...prev, destinationName]
     );
+  };
+
+  const handlePrev = () => {
+    if (currentIndex === 0) {
+      navigate(-1);
+    } else {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
+
+  const handleNext = () => {
+    if (currentIndex < state?.selectedContinents.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    } else {
+      handleComplete();
+    }
+  };
+
+  useEffect(() => {
+    const params = new URLSearchParams();
+    params.append("category", "CONTINENT");
+    if (selectedContinents.length > 0) {
+      params.append("filter", selectedContinents.join(","));
+    }
+
+    fetchData({
+      method: "GET",
+      url: APIEndPoints.DESTINATION,
+      params,
+    });
+  }, [fetchData, selectedContinents]);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.title_box}>
+        <p className={styles.title}>관심있는 여행지를 선택하세요.</p>
+      </div>
+
+      <div className={styles.destination_box}>
+        <h2 className={styles.continent_title}>{currentContinent}</h2>
+        <div className={styles.continent_section}>
+          <div className={styles.destination_list}>
+            {Object.values(groupedDestinations).map((category) => (
+              <div
+                className={styles.category_section}
+                key={category.countryName}
+              >
+                <h3 className={styles.category_title}>
+                  {category.countryName}
+                </h3>
+                <div className={styles.destination_items}>
+                  {category.destinations.map((destination) => {
+                    const isSelected = selectedDestinations.includes(
+                      destination.name
+                    );
+                    return (
+                      <div
+                        key={destination.name}
+                        className={styles.destination}
+                        onClick={() =>
+                          handleSelectDestination(destination.name)
+                        }
+                      >
+                        <div className={styles.dest_trans}>
+                          <img
+                            src={destination.imageUrl}
+                            alt={destination.name}
+                            className={`${styles.dest_img} ${
+                              isSelected ? styles.selected_img : ""
+                            }`}
+                          />
+                          {isSelected ? (
+                            <BiSolidPlaneAlt className={styles.check_box} />
+                          ) : null}
+
+                          <img
+                            src={destination.imageUrl}
+                            alt={destination.name}
+                            className={styles.dest_back_img}
+                          />
+                          {isSelected ? (
+                            <FaCheck className={styles.check_box_back} />
+                          ) : null}
+                          <div className={styles.dest_back_box}>
+                            <h1>{destination.description}</h1>
+                          </div>
+                        </div>
+                        <div className={styles.destination_text}>
+                          {destination.name}
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.button_box}>
+        <Button size="lg" variant="outline" onClick={handlePrev}>
+          이전
+        </Button>
+        <p className={styles.index_number}>
+          {currentIndex + 1} / {selectedContinents.length}
+        </p>
+        <Button size="lg" variant="outline" onClick={handleNext}>
+          {currentIndex === selectedContinents.length - 1 ? "완료" : "다음"}
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default PrefDestination;
