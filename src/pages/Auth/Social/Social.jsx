3796091c2079d1a7e7baab77a8f3fc121fa4781b
JunMinGeun <80248856+Jun-min-geun@users.noreply.github.com>
2025-03-23 18:23:03 +0900

    feat: 소셜로그인시 preference 연결결

--- src/pages/Auth/Social/Social.jsx
+++ src/pages/Auth/Social/Social.jsx
@@ -9,14 +9,17 @@ import { useAxios } from "@/hooks";
 import { useToast } from "@/contexts";
 import { useCallback, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import { AuthService } from "@/apis";
 
 const SocialPage = ({fetchUrl}) => {
   const { response: loginInfo, fetchData: fetchLogin } = useAxios();
+  const { fetchData: fetchPrefer } = useAxios();
   const navigate = useNavigate();
   const [duplicateCheck, setDuplicateCheck] = useState({
     nickname: false,
   });
   const { createToast } = useToast();
+  const { getUserInfo } = AuthService;
 
   const location = useLocation();
   const params = new URLSearchParams(location.search);
@@ -24,21 +27,44 @@ const SocialPage = ({fetchUrl}) => {
 
   const redirectPath = location.state?.from || PageEndPoints.HOME;
 
-  useEffect(()=> {
-    fetchLogin({
-      method: "GET",
-      url: fetchUrl,
-      params: { code },
-    }).then((res) => {
-      console.log(res.data);
-      localStorage.setItem("access-token", res.data?.accessToken);
-      localStorage.setItem("refresh-token", res.data?.refreshToken);
+  const handlePrefer = useCallback(async (token) =>{
+    if(!token) return;
 
-      navigate(PageEndPoints.HOME);
-    }).catch((err)=>{
-      console.log(err);
-    })
-  },[fetchLogin, code])
+    fetchPrefer({
+      method: "GET",
+      url: APIEndPoints.PREFER_DEST,
+    }).then((res) => {
+      if (Array.isArray(res.data) && res.data.length === 0) {
+        navigate(PageEndPoints.PREF_CONT, {
+          replace: true,
+          state: { mode: "create" },
+        });
+      } else {
+        const redirectPath = location.state?.from || PageEndPoints.HOME;
+        navigate(redirectPath, { replace: true });
+      }
+    });
+  },[fetchPrefer, navigate, redirectPath])
+
+  const fetchSocial = useCallback(
+    async () => {
+      fetchLogin({
+        method: "GET",
+        url: fetchUrl,
+        params: { code },
+      }).then(async (res) => {
+        localStorage.setItem("access-token", res.data?.accessToken);
+        localStorage.setItem("refresh-token", res.data?.refreshToken);
+        
+        await handlePrefer(res.data?.accessToken);
+      }).catch((err)=>{
+        console.log(err);
+      })
+    }, [fetchLogin, fetchUrl, code, handlePrefer])
+
+  useEffect(()=> {
+    fetchSocial();
+  },[fetchSocial])
 
   return (
     <div className={styles.container}>
