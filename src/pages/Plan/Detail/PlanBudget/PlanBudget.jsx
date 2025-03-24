6945feb388a0c8ad2320f2efd38e664e89c693d3
Yoonhwi <ush0105@naver.com>
2025-03-24 21:06:05 +0900

    feat: PlanBudget 영수증UI

--- src/pages/Plan/Detail/PlanBudget/PlanBudget.jsx
+++ src/pages/Plan/Detail/PlanBudget/PlanBudget.jsx
@@ -31,44 +31,82 @@ const PlanBudget = () => {
     <div className={styles.container}>
       <p className={styles.title}>예산을 한눈에 확인해보세요</p>
 
-      <div className={styles.budget_row_container}>
-        <div className={styles.budget_container}>
-          <div className={styles.top} />
-          <div className={styles.wrapper}>
-            <div className={styles.flex_column}>
-              <p className={styles.budget_title}>총 예산</p>
-              <p>{formatPrice(tripDetail.budget)}원</p>
-            </div>
+      <div className={styles.invoice_container}>
+        <div className={styles.invoice}>
+          <div className={styles.invoice_detail}>
+            <p className={styles.info}>예산 요약</p>
 
-            {Object.keys(cost).map((item) => {
-              const key = map[item];
-              if (!key) return null;
-              return (
-                <div key={key} className={styles.flex_column}>
-                  <p className={styles.budget_title}>{key}</p>
-                  <p>{formatPrice(cost[item])}원</p>
-                </div>
-              );
-            })}
-
-            <div className={styles.flex_column}>
-              <p className={styles.budget_title}>남은 예산</p>
-              <p className={styles.remain_price}>{formatPrice(remain)}원</p>
+            <div className={styles.summary}>
+              <div className={styles.summary_item}>
+                <span>총 예산</span>
+                <span>{formatPrice(tripDetail.budget)}원</span>
+              </div>
+              <div className={styles.summary_item}>
+                <span>사용 금액</span>
+                <span>{formatPrice(tripDetail.budget - remain)}원</span>
+              </div>
+              <div className={styles.summary_item}>
+                <span>남은 예산</span>
+                <span className={styles.remain}>{formatPrice(remain)}원</span>
+              </div>
             </div>
+          </div>
+
+          <div className={styles.invoice_detail}>
+            <p className={styles.info}>예약 항목별 금액 상세 내역</p>
+
+            <table className={styles.table}>
+              <thead>
+                <tr>
+                  <th>예산 항목</th>
+                  <th>금액</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {Object.keys(cost).map((item) => {
+                  const key = map[item];
+                  if (!key) return null;
+
+                  return (
+                    <tr className={styles.service} key={key}>
+                      <td className={styles.table_item}>{key}</td>
+                      <td className={styles.table_item}>
+                        {formatPrice(cost[item])}원
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
           </div>
         </div>
 
-        <div className={styles.budget_item}>
-          <div className={styles.top} />
-          <div className={styles.wrapper}>
-            {flattendItinerary.map((item) => {
-              return (
-                <div key={item.date} className={styles.flex_column}>
-                  <p className={styles.budget_title}>{item.day}일차</p>
-                  <p>{formatPrice(item.cost)} 원</p>
-                </div>
-              );
-            })}
+        <div className={styles.invoice}>
+          <div className={styles.invoice_detail}>
+            <p className={styles.info}>일정별 예산 상세 내역</p>
+
+            <table className={styles.table}>
+              <thead>
+                <tr>
+                  <th>일정</th>
+                  <th>금액</th>
+                </tr>
+              </thead>
+
+              <tbody>
+                {flattendItinerary.map((item) => (
+                  <tr className={styles.service} key={item.date}>
+                    <td className={styles.table_item}>
+                      {item.day}일차 ({item.date})
+                    </td>
+                    <td className={styles.table_item}>
+                      {formatPrice(item.cost)}원
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
