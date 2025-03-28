0905daf8150bce41357622a84520ee61824a1169
Yoonhwi <ush0105@naver.com>
2025-03-28 19:03:13 +0900

    feat: Search페이지 react-hook-form추가

--- src/pages/Search/SearchLayout.jsx
+++ src/pages/Search/SearchLayout.jsx
@@ -7,6 +7,9 @@ import { Route, Routes, useLocation, useNavigate } from "react-router-dom";
 import SearchDetail from "./Detail/SearchDetail";
 import SearchMain from "./Main/SearchMain";
 import styles from "./SearchLayout.module.css";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { searchScheme } from "./constants";
 
 const SearchLayout = () => {
   const location = useLocation();
@@ -14,13 +17,17 @@ const SearchLayout = () => {
   const params = new URLSearchParams(location.search);
   const keyword = params.get("keyword");
 
-  const exitTransitionX = keyword ? "50%" : "-50%";
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    resolver: zodResolver(searchScheme),
+  });
 
   const handleSearch = useCallback(
-    (e) => {
-      e.preventDefault();
-      const keyword = e.target[0].value;
-      e.target[0].value = "";
+    (data) => {
+      const keyword = data.keyword.trim();
 
       if (keyword) {
         navigate(`?keyword=${keyword}`);
@@ -32,38 +39,45 @@ const SearchLayout = () => {
   return (
     <BaseLayout>
       <div className={styles.container}>
-        <div className={styles.search_container}>
-          <form className={styles.search_input} onSubmit={handleSearch}>
-            <Input
-              size="lg"
-              placeholder="Search topics ..."
-              style={{
-                width: "100%",
-                borderRadius: "28px",
-                padding: "0.7rem 4rem 0.7rem 1.5rem",
-                boxSizing: "border-box",
-              }}
-            />
-
-            <div className={styles.icon_search_box}>
-              <Button variant="none" type="submit">
-              <FiSearch
-                size={24}
-                className={styles.icon_search}
-                
+        <div className={styles.search_field}>
+          <div className={styles.search_container}>
+            <form
+              className={styles.search_input}
+              onSubmit={handleSubmit(handleSearch)}
+            >
+              <Input
+                size="lg"
+                placeholder="Search topics ..."
+                style={{
+                  width: "100%",
+                  borderRadius: "28px",
+                  padding: "0.7rem 4rem 0.7rem 1.5rem",
+                  boxSizing: "border-box",
+                }}
+                register={register}
+                name="keyword"
               />
-              </Button>
-            </div>
-          </form>
+
+              <div className={styles.icon_search_box}>
+                <Button variant="none" type="submit">
+                  <FiSearch size={24} className={styles.icon_search} />
+                </Button>
+              </div>
+            </form>
+          </div>
+
+          {errors.keyword && (
+            <p className={styles.error_message}>{errors.keyword.message}</p>
+          )}
         </div>
 
         <AnimatePresence mode="wait">
           <motion.div
             key={location.pathname + location.search}
             className={styles.content}
-            initial={{ opacity: 0, x: exitTransitionX }}
+            initial={{ opacity: 0, x: "-10%" }}
             animate={{ opacity: 1, x: 0 }}
-            exit={{ opacity: 0, x: exitTransitionX }}
+            exit={{ opacity: 0, x: "10%" }}
             transition={{ duration: 0.5 }}
           >
             <Routes location={location}>
